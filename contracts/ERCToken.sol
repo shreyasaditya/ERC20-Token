eb5d842f83254a62511d9635d9a95b647e98cd6e
Shreyas Aditya <ashreyas@seas.upenn.edu>
2018-05-26 22:06:52 -0400

    Implemented a new ERC20 Token

---
+++ contracts/ERCToken.sol
@@ -0,0 +1,96 @@
+pragma solidity ^0.4.11;
+
+contract ERCToken {
+    /*
+     *  Data structures
+     */
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+    string token_name;
+    string token_symbol;
+    uint8 token_dec;
+    uint256 token_supply;
+
+    // Constructor
+    function ERCToken (uint _totalSupply) public {
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
+        token_name = "testtoken";
+        token_symbol = "TTK";
+        token_dec = 5;
+        
+    }
+    
+    function name() public constant returns (string t_name){
+        return token_name;
+    }
+    
+    function symbol() public constant returns (string t_symbol){
+        return token_symbol;
+    }
+    
+    function decimals() public constant returns (uint8 t_decimals){
+        return token_dec;
+    }
+    
+    function totalSupply() public constant returns (uint256 t_totalSupply){
+        return totalSupply;
+    }
+
+    /*
+     *  Read and write storage functions
+     */
+    /// @dev Transfers sender's tokens to a given address. Returns success.
+    /// @param _to Address of token receiver.
+    /// @param _value Number of tokens to transfer.
+    function transfer(address _to, uint256 _value)  public returns (bool success) {
+        if(balances[msg.sender] >= _value){
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            
+            return true;
+        }
+        
+        return false;
+        
+    }
+
+    /// @dev Allows allowed third party to transfer tokens from one address to another. Returns success.
+    /// @param _from Address from where tokens are withdrawn.
+    /// @param _to Address to where tokens are sent.
+    /// @param _value Number of tokens to transfer.
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        if(allowed[_from][msg.sender] >= _value){
+            allowed[_from][msg.sender] -= _value;
+            balances[_from] -= _value;
+            balances[_to] += _value;
+        
+            return true;
+        }
+        return false;
+       
+    }
+
+    /// @dev Returns number of tokens owned by given address.
+    /// @param _owner Address of token owner.
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    /// @dev Sets approved amount of tokens for spender. Returns success.
+    /// @param _spender Address of allowed account.
+    /// @param _value Number of approved tokens.
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /// @dev Returns number of allowed tokens for given address.
+    /// @param _owner Address of token owner.
+    /// @param _spender Address of token spender.
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+}
